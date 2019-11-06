6489a878f2ec7b992df385e829a5cf836be760f5
Isaac Avilez <isaac1180@aol.com>
2019-11-06 16:44:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,15 +1,15 @@
 def using_push(array, element)    
-  array.push(element)
+  return array.push(element)
 end
 
 def using_unshift(array, string) 
-  array.unshift(string)
+ return array.unshift(string)
 end
 
 def using_pop(array)  
-  array.pop()
+  return array.pop()
 end
 
 def pop_with_args(array)
-  array.pop([-1..-2)
+  return array.pop([-1..-2)
 end
